e48b600c3edee8a5add0c9f08aa1f653d5379d7b
Sergey Lyulchenko <solo300m@yandex.ru>
2023-04-13 12:27:13 +0400

    homwork 1-etalon

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -2,136 +2,71 @@ import React, {useState} from 'react';
 import API from "../api/API";
 
 export const Users = () => {
-    const arrUsers = API.users.fetchAll();
-    const [users, setUser] = useState(arrUsers);
-    //console.log(arrUsers[0]);
+    const [users, setUsers] = useState(API.users.fetchAll());
 
-    const formatCount = () => {
-        let count = users.length;
-        return count === 0 ? 'Ни кто не ' : count;
-    }
+    const handleDelete = (userId) => {
+        setUsers(users.filter((user) => user._id !== userId));
+    };
 
-    const formatHuman = (count) => {
-        console.log(count);
-        
-        if(Math.floor(count/10) === 0 && count%10 === 1) return 'человек';
-        if(Math.floor(count/10) === 0 && count%10 === 2) return 'человека';
-        if(Math.floor(count/10) === 0 && count%10 === 3) return 'человека';
-        if(Math.floor(count/10) === 0 && count%10 === 4) return 'человека';
-        if(Math.floor(count/10) === 0 && (count%10 > 4 && count%10 < 10)) return 'человек';
-        if(Math.floor(count/10) === 1 && (count%10 >= 0  && count%10 <= 10)) return 'человек';
-        if(Math.floor(count/10) > 1 && count%10 === 1) return 'человек';
-        if(Math.floor(count/10) > 1 && count%10 === 2) return 'человека';   
-        if(Math.floor(count/10) > 1 && count%10 === 3) return 'человека';  
-        if(Math.floor(count/10) > 1 && count%10 === 4) return 'человека';  
-                 
-    }
-        
-    
-
-    const getHeadTable = () => {
-        return (
-            <tr>
-                <th scope="col">№ п/п</th>    
-                <th scope="col">Имя</th>
-                <th scope="col">Качества</th>
-                <th scope="col">Профессия</th>
-                <th scope="col">Встретился, раз</th>
-                <th scope="col">Оценка</th>
-                <th scope="col">Удалить</th>
-            </tr>
-        );
-    }
-
-    const setColorBadge = (color) =>{
-        let classes = 'badge m-2 p-2 shadow text-center fw-normal ';
-        switch (color) {
-            case 'Нудила':{
-                classes += 'bg-primary';
-                break;
-            }
-            case 'Странный':{
-                classes += 'bg-secondary';
-                break;
-            }
-            case 'Троль':{
-                classes += 'bg-success';
-                break;
-            }
-            case 'Алкоголик':{
-                classes += 'bg-danger';
-                break;
-            }
-            case 'Красавчик':{
-                classes += 'bg-info';
-                break;
-            }
-            case 'Неуверенный':{
-                classes += 'bg-dark';
-                break;
-            }
-            default:{
-                classes += 'bg-primary';
-                break;
-            }
-        }
-        return classes;
-    }
-
-    const handleDeleteRow = (id) => {
-        //console.log(id);        
-        setUser(prevState => prevState.filter(users => users !== id));
-        //formatHuman(users.length - 1);
-        
-    }
-    
-    const getRowsTable = () => {
-        return users.map((user, index) => (
-            <tr>
-                <th scope="row">{index+1}</th>
-                <td key={index}>{user.name}</td>
-                <td key={index}>{
-                    user.qualities.map((quality) => (
-                        <span className={setColorBadge(quality.name)}>{quality.name}</span>
-                    ))
-                }</td>
-                <td key={index}>{user.profession.name}</td>
-                <td key={index}>{user.completedMeetings}</td>
-                <td key={index}>{user.rate}</td>
-                <td key={index}>{
-                    <button className='btn btn-danger shadow' onClick={() => handleDeleteRow(user)
-                    }>Delete</button>
-                }</td>
-            </tr>
-        ));
-    }
-
-    const getTable = () => {
-        return (            
-            <table className='table table-striped'>
-                <thead>
-                    {getHeadTable()}
-                </thead>
-                <tbody className='table-group-divider'>
-                    {getRowsTable()}                                    
-                </tbody>
-            </table>
-        )
-    }
-
-    const getTitleInfo = () => {
-        return (
-            <div>        
-                <span className='badge bg-primary pb-2 shadow text-center fs-5 fw-normal'>{formatCount()} {formatHuman(users.length)} тусанет с тобой сегодня!</span>
-            </div>
-        );
-
-    }
+    const renderPhrase = (number) => {
+        const lastOne = Number(number.toString().slice(-1));
+        if (number > 4 && number < 15) return "человек тусанет";
+        if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут";
+        if (lastOne === 1) return "человек тусанет";
+        return "человек тусанет";
+    };
 
     return (
         <>
-            {getTitleInfo()}
-            {getTable()}
+            <h2>
+                <span
+                    className={"badge " + (users.length > 0 ? "bg-primary" : "bg-danger")}
+                >
+                    {users.length > 0
+                        ? `${users.length + " " + renderPhrase(users.length)} с тобой сегодня`
+                        : "Никто с тобой не тусанет"}
+                </span>
+            </h2>
+
+            {users.length > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Профессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                            <tr key={user._id}>
+                            <td>{user.name}</td>
+                            <td>
+                                {user.qualities.map((item) => (
+                                    <span className={"badge m-1 bg-" + item.color} key={item._id}>
+                                        {item.name}
+                                    </span>
+                                ))}
+                            </td>
+                            <td>{user.profession.name}</td>
+                            <td>{user.completedMeetings}</td>
+                            <td>{user.rate} /5</td>
+                            <td>
+                                <button
+                                    onClick={() => handleDelete(user._id)}
+                                    className="btn btn-danger"
+                                >
+                                    delete
+                                </button>
+                            </td>
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </>
-    )
-}
+    );
+};
